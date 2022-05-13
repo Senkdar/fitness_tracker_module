25cf27b84c20953a01fc3b3725bbc7056ccb7b62
Алексей Кринкин <42-80-67@mail.ru>
2022-05-13 19:13:48 +0300

    Спринт 2. Проект после первого ревью

--- homework.py
+++ homework.py
@@ -1,27 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float):
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+# по поводу замечания на счет имени переменной duration:
+# pytest требует именно такое имя, иначе тест не проходит
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type};'
-                f' Длительность: {"{:.3f}".format(self.duration)} ч.;'
-                f' Дистанция: {"{:.3f}".format(self.distance)} км;'
-                f' Ср. скорость: {"{:.3f}".format(self.speed)} км/ч;'
-                f' Потрачено ккал: {"{:.3f}".format(self.calories)}.')
+        return (
+            f'Тип тренировки: {self.training_type};'
+            f' Длительность: {"{:.3f}".format(self.duration)} ч.;'
+            f' Дистанция: {"{:.3f}".format(self.distance)} км;'
+            f' Ср. скорость: {"{:.3f}".format(self.speed)} км/ч;'
+            f' Потрачено ккал: {"{:.3f}".format(self.calories)}.'
+        )
 
 
 class Training:
     """Базовый класс тренировки."""
+# здесь столкнулся с проблемой при использовании @dataclass:
+# при использовании неправильно расчитывалось расстояние в классе Swimming
     M_IN_KM = 1000
     LEN_STEP = 0.65
 
@@ -35,43 +39,48 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        distance = self.action * self.LEN_STEP / self.M_IN_KM
-        return distance
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        speed = self.get_distance() / self.duration
-        return speed
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         pass
+        raise NotImplementedError('не переопределен метод get_spent_calories')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        inf_message = InfoMessage(self.__class__.__name__,
-                                  self.duration, self.get_distance(),
-                                  self.get_mean_speed(),
-                                  self.get_spent_calories())
-        return inf_message
+        return InfoMessage(
+            self.__class__.__name__,
+            self.duration, self.get_distance(),
+            self.get_mean_speed(),
+            self.get_spent_calories()
+        )
 
 
 class Running(Training):
     """Тренировка: бег."""
+    coeff_calorie_1: int = 18
+    coeff_calorie_2: int = 20
+    minutes_in_hour: int = 60
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        coeff_calorie_1 = 18
-        coeff_calorie_2 = 20
-        minutes = 60
-        calories = ((coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2)
-                    * self.weight / self.M_IN_KM
-                    * self.duration * minutes)
-        return calories
+        return (
+            (self.coeff_calorie_1 * self.get_mean_speed()
+             - self.coeff_calorie_2)
+            * self.weight / self.M_IN_KM
+            * self.duration * self.minutes_in_hour
+        )
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    coeff_calorie_1: float = 0.035
+    coeff_calorie_2: float = 0.029
+    minutes_in_hour: int = 60
 
     def __init__(self,
                  action: int,
@@ -82,22 +91,29 @@ class SportsWalking(Training):
                          duration,
                          weight)
         self.height = height
+# правильно ли я понял, что декоратор @dataclass не применить
+# ко всем классам? не получалось вынести константу LEN_STEP
+# в тело родительского класса,
+# т.к. впоследствии у дочерних классов возникала ошибка, что
+# аргумент не по умолчанию следует за аргументом по умолчанию
+# таким образом, я не мог задать свойство height в классе SportsWalking)
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        coeff_calorie_1 = 0.035
-        coeff_calorie_2 = 0.029
-        minutes = 60
-        calories = ((coeff_calorie_1 * self.weight
-                    + (self.get_mean_speed()**2 // self.height)
-                    * coeff_calorie_2 * self.weight)
-                    * self.duration * minutes)
-        return calories
+
+        return (
+            (self.coeff_calorie_1 * self.weight
+             + (self.get_mean_speed()**2 // self.height)
+             * self.coeff_calorie_2 * self.weight)
+            * self.duration * self.minutes_in_hour
+        )
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP = 1.38
+    coeff_calorie_1: float = 1.1
+    coeff_calorie_2: float = 2
 
     def __init__(self,
                  action: int,
@@ -114,34 +130,36 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        speed = (self.length_pool * self.count_pool
-                 / self.M_IN_KM / self.duration)
-        return speed
+        return (self.length_pool * self.count_pool
+                / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        coeff_calorie_1 = 1.1
-        coeff_calorie_2 = 2
-        calories = ((self.get_mean_speed() + coeff_calorie_1)
-                    * coeff_calorie_2 * self.weight)
-        return calories
+        return (
+            (self.get_mean_speed() + self.coeff_calorie_1)
+            * self.coeff_calorie_2 * self.weight
+        )
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    if workout_type == 'SWM':
+    dictionary = {
+        Swimming: 'SWM',
+        Running: 'RUN',
+        SportsWalking: 'WLK'
+    }
+
+    if workout_type == dictionary[Swimming]:
         return Swimming(*data)
-    elif workout_type == 'RUN':
+    elif workout_type == dictionary[Running]:
         return Running(*data)
-    elif workout_type == 'WLK':
+    elif workout_type == dictionary[SportsWalking]:
         return SportsWalking(*data)
-# можно ли тут обойтись без словаря?
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info = training.show_training_info()
-    print(info.get_message())
+    print(training.show_training_info().get_message())
 
 
 if __name__ == '__main__':
